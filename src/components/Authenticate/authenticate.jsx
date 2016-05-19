d65d1c609d5eafe9b4b4703c182fcc874e29d577
dokhoa <khoa.do@eroad.com>
2016-05-19 15:32:36 +1200

    fix layout

--- src/components/Authenticate/authenticate.jsx
+++ src/components/Authenticate/authenticate.jsx
@@ -17,19 +17,19 @@ class Authenticate extends React.Component {
   }
   render() {
     return (
-      <div id="container">
+      <div className={styles.container}>
           <div id="alertsListWrapper">
           </div>
           <div id="wrapper">
-              <div id="content">
-                  <div id="loginPanel" className={styles.logp}>
+              <div className={styles.content}>
+                  <div className={styles.loginPanel}>
                       <h2>Please input your mobile number</h2>
                       <form method="post" id="authenticateForm" action="">
                           <ul className={styles.loginInstruction}>
                               <li>
                                   <input type="text" placeholder="Mobile Number" name="mobile" id="mobile" size="10" value=""/>
                               </li>
-                                  <li className={styles.loginButtonContainer}><input id="submitButton" name="submit" className={styles.button} type="submit" value="SUBMIT" /></li>
+                                  <li className={styles.loginButtonContainer}><input id="submitButton" name="submit" className={styles.loginbutton} type="submit" value="SUBMIT" /></li>
                               </ul>
                       </form>
                   </div>
